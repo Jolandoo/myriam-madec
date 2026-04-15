49de5dbf0eb50a1266182e247a805c28cb17a869
Kuroro Lucifer <madecjolann@gmail.com>
2026-04-15 20:41:28 +0200

    Fix : No video

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
           playsInline
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src="/videos/hero.mp4" type="video/mp4" />
+          <source src="/videos/hero-web.mp4" type="video/mp4" />
         </video>
 
       </div>
